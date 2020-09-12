ccf1c0e6d387ac4db373f09a0240ce6500e2e0a6
unknown <C.Berumen51@gmail.com>
2020-09-11 19:01:48 -0700

    near MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -10,6 +10,12 @@ PUSH =  0b01000101
 POP  =  0b01000110
 CALL =  0b01010000
 RET  =  0b00010001
+CMP  =  0b10100111
+JMP  =  0b01010100
+JEQ  =  0b01010101
+JNE  =  0b01010110
+ADD  =  0b10100000
+
 
 
 class CPU:
@@ -21,7 +27,8 @@ class CPU:
         self.ram = [0] * 256
         self.pc = 0
         self.registers = [0] * 8
-        self.SP = 7
+        self.sp = 7
+        self.fl = 6
         self.running = False
 
         self.operations = {}
@@ -31,7 +38,15 @@ class CPU:
         self.operations[PRN] = self.handle_PRN
         self.operations[PUSH] = self.handle_PUSH
         self.operations[POP] = self.handle_POP
-        # self.operations[CALL] = self.handle_CALL
+        self.operations[CALL] = self.handle_CALL
+        self.operations[RET] = self.handle_RET
+        self.operations[CMP] = self.handle_CMP
+        self.operations[JMP] = self.handle_JMP
+        self.operations[JEQ] = self.handle_JEQ
+        self.operations[JNE] = self.handle_JNE
+        self.operations[ADD] = self.handle_ADD
+        
+
          
 
     
@@ -56,19 +71,24 @@ class CPU:
         #     self.ram[address] = instruction
         #     address += 1
 
-        try:
-            with open(file_name) as file:
-                for line in file:
-                    split_file = line.split("#")[0]
-                    comm = split_file.strip()
-                    if comm == "":
-                        continue
-                    instruction = int(comm, 2)
-                    self.ram[address] = instruction
-                    address += 1
-        except FileNotFoundError:
-            print(f"{sys.argv[0]} {sys.argv[1]} file not found")
-            sys.exit()   
+        if len(sys.argv) != 2:
+            print("usage: ls8.py filename")
+            sys.exit(1)
+
+        progname = sys.argv[1]
+        address = 0
+
+        with open(progname) as f:
+            for line in f:
+                line = line.split("#")[0]
+                line = line.strip()
+                if line == "":
+                    continue
+
+                value = int(line, 2)
+                # set the binary instruction to memory
+                self.ram[address] = value
+                address += 1  
 
     def ram_read(self, MAR):
         return self.ram[MAR]
@@ -81,14 +101,19 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
-        if op == "AND":
-            if reg_a == 1 and reg_b == 1:
-                return True
-            else:
-                return False
-        if op == "MUL":
-            self.reg[reg_a] = self.reg[reg_a] * self.reg[reg_b]
+        elif op == "SUB":
+            self.reg[reg_a] -= self.reg[reg_b]
+        elif op == 'MUL':
+            self.reg[reg_a] *= self.reg[reg_b]
+        elif op == 'DIV':
+            self.reg[reg_a] /= self.reg[reg_b]
+        elif op == "CMP":
+            if self.reg[reg_a] == self.reg[reg_b]:
+                self.fl = 0b00000001
+            if self.reg[reg_a] < self.reg[reg_b]:
+                self.fl = 0b00000010
+            if self.reg[reg_a] > self.reg[reg_b]:
+                self.fl = 0b00000011
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -136,10 +161,47 @@ class CPU:
         self.reg[7] += 1
         self.pc += ir_len
 
-    # def handle_CALL(self):
-    #     ir = self.ram_read(self.pc)
-    #     val = ir
-    #     op_
+    def handle_CALL(self):
+        given_reg = self.ram[self.pc + 1]
+        self.sp -= 1
+        self.ram[self.sp] = self.pc + 2
+        self.pc = self.reg[given_reg]
+
+    def handle_RET(self):
+        self.pc = self.ram[self.sp]
+        self.reg[7] += 1
+
+    def handle_CMP(self):
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+        self.alu('CMP', operand_a, operand_b)
+        self.pc += 3
+
+    def handle_JMP(self):
+        reg_index = self.ram_read(self.pc + 1)
+        self.pc = self.reg[reg_index]
+
+    def handle_JEQ(self):
+        if self.fl == 0b00000001:
+            reg_index = self.ram_read(self.pc + 1)
+            self.pc = self.reg[reg_index]
+        else:
+            self.pc += 2
+
+    def handle_JNE(self):
+        if self.fl != 0b00000001:
+            reg_index = self.ram_read(self.pc + 1)
+            self.pc = self.reg[reg_index]
+        else:
+            self.pc += 2
+    def handle_ADD(self):
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+        self.alu('ADD', operand_a, operand_b)
+        self.pc += 3
+
+
+
 
     def trace(self):
         """
